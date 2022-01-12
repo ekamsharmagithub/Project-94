4f791cb5f512f7780f1b7fe6ad41db3861c08c5c
ekamsharmagithub <90507473+ekamsharmagithub@users.noreply.github.com>
2022-01-12 17:09:34 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,7 +1,9 @@
-function addUser() {
 
-    user_name = document.getElementById("user_name").value;
+function adduser() {
 
-    localStorage.setItem("user_name", user_name);
-      window.location = "kwitter_room.html";      
+  user_name = document.getElementById("user_name").value;
+
+  localStorage.setItem("Username", user_name);
+  
+    window.location = "kwitter_room.html";
 }
--- kwitter_room.js
+++ kwitter_room.js
@@ -13,17 +13,45 @@ const firebaseConfig = {
       appId: "1:878927699302:web:e6283b21e2e1d92be6bca7",
       measurementId: "G-F388FD88B2"
     };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("Username");
+
+document.getElementById("welcome_user_name").innerHTML = "Welcome " + user_name + "!";
+
+
+
+function addroom() {
+      room_name = document.getElementById("room_name").value;
+
+     localStorage.setItem("Roomname",room_name);
+ 
+     window.location = "kwitter_page.html";
+
+     firebase.database().ref("/").child(room_name).update({
+        purpose: "Adding Room Name"
+  });
+}
+
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+          window.location = "index.html";
+      }
+      
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
-      function addUser()
-{
-    user_name = document.getElementById("user_name").value;
-    firebase.database().ref("/").child(user_name).update({
-        purpose : "adding user"
-    });
-}
-      //End code
-      });});}
+      Room_names = childKey;
+     //Start code
+     
+    
+     //End code
+     });});}
 getData();
+
+
+
+
+
